8aa0b6105fe3e5327db60fa41b2d9ad98ac5fe66
Juan García <duartejj@uninorte.edu.co>
2020-09-23 10:36:12 -0500

    escritura a base de datos con UDP

--- indexv2.js
+++ indexv2.js
@@ -75,12 +75,12 @@ udpserver.on('error', (err)=>{
     
             var gpsinfo = latitud+"/"+longitud+"/"+stamptime;
     
-            /* truckdata = {latitud: latitud, longitud: longitud, stamptime: stamptime}
+            truckdata = {latitud: latitud, longitud: longitud, stamptime: stamptime}
             let sql = 'INSERT INTO gpsdata SET ?';
     
             let query = database.query(sql,truckdata,(err,result) =>{
                 if(err) throw err;
-            }) */
+            })
             
             
             fs.writeFile('coordenadas.txt', gpsinfo, function(error){
