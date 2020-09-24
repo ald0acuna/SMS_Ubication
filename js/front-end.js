b2a97916e4336726bb62f6072f77d877c9aa6653
Juan García <duartejj@uninorte.edu.co>
2020-09-23 23:32:20 -0500

    polilinea

--- js/front-end.js
+++ js/front-end.js
@@ -3,7 +3,7 @@
 const tilesprovider = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 var myMap = L.map('mymap').setView([11.01756,-74.85698], 13);
 
-
+var i = 1;
 
 L.tileLayer(tilesprovider,{
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -19,8 +19,11 @@ var mymarker = L.icon({
 
 })
 
+
+
 function readFile(){
     jQuery.get('../coordenadas.txt', function(txt){
+
         lat=  txt.toString('utf8').split("/")[0];
         $("#caja-latitude").text(lat);
         lon =txt.toString('utf8').split("/")[1];
@@ -28,9 +31,9 @@ function readFile(){
         sT = txt.toString('utf8').split("/")[2];
         $("#caja-stamptime").text(sT);
         marker = L.marker([lat,lon]).addTo(myMap)
-
+        
     })
-
+    var cArray = [];
     setInterval(function(){ 
         jQuery.get('../coordenadas.txt', function(txt){
             lat=  txt.toString('utf8').split("/")[0];
@@ -40,12 +43,16 @@ function readFile(){
             sT = txt.toString('utf8').split("/")[2];
             $("#caja-stamptime").text(sT);
         
-            marker = L.marker([lat,lon]).addTo(myMap)
-            
+            marker.setLatLng([lat, lon]).update();
+            var coor = [lat,lon];
+            cArray.push(coor)
+            line(cArray);
         })
         
 
     },1000);
-
+    function line(cArray){
+        var polyline =L.polyline(cArray, {color: 'red'}).addTo(myMap);
+    }
 };
 
